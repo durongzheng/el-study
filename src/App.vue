62cffdf0d2d6c820d65deef1b111f3c9ee74a344
durongzheng <drz@easyman.com.cn>
2020-03-19 09:03:57 +0800

    init.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Buttons/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Buttons from './components/Buttons'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Buttons
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
